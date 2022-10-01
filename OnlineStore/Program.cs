0668f4361682f704ed556d06197ddfdebab27826
Kirill Lagutin <lagutin.k@mail.ru>
2022-10-02 01:26:27 +0300

    Add task 3

--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -1,24 +1,52 @@
+/*
+1 Добавьте Swagger к своему проекту
+2 Сделайте добавление товаров в каталог потокобезопасным
+3 Реализуйте потокобезопасное чтение товаров из каталога
+4 * Реализуйте потокобезопасный каталог с использованием потокобезопасной коллекции
+5 * Реализуйте потокобезопасное удаление товара из каталога
+*/
+
 using Microsoft.AspNetCore.Http.Json;
 using OnlineStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.Configure<JsonOptions>(options => options.SerializerOptions.WriteIndented = true);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.Configure<JsonOptions>(options => options.SerializerOptions.WriteIndented = true);
 var app = builder.Build();
 
-app.MapGet("/catalog", () => Catalog.phones);
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.MapPost("/catalog/add_product", (Phone phone, HttpContext context) =>
+var tabs = new Tabs();
+var phones = new Phones();
+
+/* Использование потокобезопасной коллекции */
+app.MapGet("/catalog/get_tabs", () => tabs.GetTabs());
+
+app.MapPost("/catalog/add_tab", (Tab tab) => tabs.AddTabs(tab));
+
+app.MapGet("/catalog/delete_tab", (int id) => tabs.DeleteTab(id));
+
+
+/*Использование lock*/
+app.MapGet("/catalog/get_phones", () => phones.GetPhones());
+
+app.MapPost("/catalog/add_phone", (Phone phone, HttpContext context) =>
 {
-    Catalog.phones.Add(phone);
+    phones.AddPhone(phone);
     context.Response.StatusCode = 201;
 });
 
-app.MapPost("catalog/clear_catalog", (HttpContext context) =>
+app.MapPost("catalog/clear_phones", (HttpContext context) =>
 {
-    Catalog.phones.Clear();
+    phones.ClearPhones();
     context.Response.StatusCode = 205;
 });
-    
+
 app.MapGet("/headers", (HttpContext context) => context.Request.Headers);
+
 
 app.Run();
